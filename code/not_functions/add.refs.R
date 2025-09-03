39018c221f42b7931c37ced242ac8fe614d524e3
spbruneel-INBO <stijn.bruneel@inbo.be>
2025-09-03 12:37:45 +0200

    Create add.refs.R

---
+++ code/not_functions/add.refs.R
@@ -0,0 +1,10 @@
+INBOmd::add_report_numbers(
+  path="./",
+  doi="10.21436/inbor.132763058",
+  year=2025,
+  reportnr=54,
+  depotnr="D/2025/3241/315",
+  photo="./media/coverpic.JPG",
+  description="Het sluisstuwcomplex van Balgerhoeke op het Afleidingskanaal van de Leie. Foto: David Buysse (INBO)")
+
+INBOmd::validate_doi("10.21436/inbor.132763058")
